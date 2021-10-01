a85129064dad550ece7bd60f0fe24ef08c6c4359
rqtrinh <rqtrinh@cpp.edu>
2021-09-30 23:53:22 -0700

    Finished working on code for 1.a) of assignment that can search for entry based on phone number or salary

--- employeeMain.c
+++ employeeMain.c
@@ -6,13 +6,64 @@ int main(void)
     //defined in employeeSearchOne.c
     PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char* nameToFind);
+    PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char* phoneToFind);
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);
 
     //defined by employeeTable.c
     extern Employee EmployeeTable[];
     extern const int EmployeeTableEntries;
 
     PtrToEmployee matchPtr; //Declaration
+    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045);
+
+    //searchEmployeeByNumber
+    //Example not found
+    if (matchPtr != NULL)
+        printf("Employee ID 1045 is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee ID 1045 is NOT found in the record\n");
     
+    //searchEmployeeByName
+    //Example found
+    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+    if (matchPtr != NULL)
+        printf("Employee Tony Bobcat is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee Tony Bobcat is NOT found in the record\n");
+
+
+    printf("\n");
+    //searchEmployeeByPhone
+    //Example found
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-555-2749");
+    if (matchPtr != NULL)
+        printf("Employee number 714-555-2749 is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee number 714-555-2749 is NOT found in the record\n");
+
+    //Example not found
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "222-555-2749");
+    if (matchPtr != NULL)
+        printf("Employee number 222-555-2749 is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee number 222-555-2749 is NOT found in the record\n");
+
+    printf("\n");
+    //searchEmployeeBySalary
+    //Example found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 7.80);
+    if (matchPtr != NULL)
+        printf("Employee salary 7.80 is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee salary 7.80 is NOT found in the record\n");
+
+    //Example not found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 10.80);
+    if (matchPtr != NULL)
+        printf("Employee salary 10.80 is in record %d\n", matchPtr-EmployeeTable);
+    else
+        printf("Employee salary 10.80 is NOT found in the record\n");
+
 
     return EXIT_SUCCESS;
 }
--- employeeOne.c
+++ employeeOne.c
@@ -18,9 +18,32 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char* 
     const PtrToConstEmployee endPtr = ptr + tableSize;
     for(; ptr < endPtr; ptr++)  //search until end of table 
     {
-        if(strcmp(ptr->name,targetName) == 0) //check if it equals the Employee number
-            return (PtrToEmployee) ptr; //return location of the number to callee
+        if(strcmp(ptr->name,targetName) == 0) //check if it equals the Employee name
+            return (PtrToEmployee) ptr; //return location of the name to callee
     }
-    return NULL; //this will happen if no Employee number matches in loop above
+    return NULL; //this will happen if no Employee name matches in loop above
 }
 
+//Essentially the same functionality as above but comparing strings to check if equal
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char* targetPhone)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)  //search until end of table 
+    {
+        if(strcmp(ptr->phone,targetPhone) == 0) //check if it equals the Employee phone number
+            return (PtrToEmployee) ptr; //return location of the phone number to callee
+    }
+    return NULL; //this will happen if no Employee phone number matches in loop above
+}
+
+//Essentially the same functionality as above but comparing strings to check if equal
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)  //search until end of table 
+    {
+        if(ptr->salary == targetSalary) //check if it equals the Employee salary
+            return (PtrToEmployee) ptr; //return location of the salary to callee
+    }
+    return NULL; //this will happen if no Employee salary matches in loop above
+}
